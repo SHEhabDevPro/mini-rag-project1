a701fc45652d8e6d4e06e1647652b32b29a932df
SHEhabDevPro <shehab.farrag@ejust.edu.eg>
2026-02-02 17:02:22 +0200

    update all

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
+from routes import base
 app = FastAPI()
 
-@app.get("/")
-
-@app.get("/welcome")
-def root():
-    return {"status": "🔥 FastAPI is alive"}
+app.include_router(base.base_router)
+ 
